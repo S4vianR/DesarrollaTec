f1c0a5cc81f761b82b70b8ea03bd1a6bd76c6bc3
S4vianR <reuben4.com@gmail.com>
2024-11-26 16:50:57 -0700

    Se agregarón varias imágenes, se modificaron los archivos de: layout e index, además se agregó el componente de Nav y se agregó la información del punto 1. Ecosistemas principales en Chihuahua

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{html,js,jsx,md,mdx,svelte,ts,tsx,vue,astro}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'roboto': ['Roboto', 'sans-serif'],
+			},
+			colors: {
+				'orange-accent-1': '#DD8938',
+				'orange-accent-2': '#BD8B5B',
+				'green-accent-1': '#5F6456',
+				'purple-accent-1': '#382934',
+				'pink-accent-1': '#FAA0A0',
+				'pink-accent-2': '#905547',
+				'pink-accent-3': '#e9a492',
+				'pink-accent-4' : '#A95C68',
+				'yellow-sticky-note': '#EBDF9A',
+				'blue-sticky-note': '#B6D0E2',
+				'green-sticky-note': '#A5D6A7',
+				'pink-sticky-note': '#F48FB1',
+			}
+		},
 	},
 	plugins: [],
 }
